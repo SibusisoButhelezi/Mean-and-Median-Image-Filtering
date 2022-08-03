61d0d4b1f0cd5b3eeb9b915b99c4731ea72f9d11
Sibusiso Buthelezi <sbusiso11buthelezi@gmail.com>
2022-08-03 16:43:06 +0200

    Added more processing methods to the Utilities.java class, worked on MeanFilterSerial.java

--- src/MeanFilterSerial.java
+++ src/MeanFilterSerial.java
@@ -8,20 +8,52 @@ public class MeanFilterSerial{
 
 	public static void main(String[] args){
 
-		BufferedImage img = null;
-		img = Utilities.readInImage(img);
+		int dimension = 9;
+		BufferedImage img = null;//, newImg = null;
+		img = Utilities.readImage(img);
+		//newImg = Utilities.readImage(img);
+
+		// int[] borders = Utilities.getBorders(img, dimension);
+
+		// int radius = dimension/2;
+
+		// int topBorder = borders[0];
+		// int rightBorder = borders[1];
+		// int bottomBorder = borders[2];
+		// int leftBorder = borders[3];
+
+		// int[][] pixels = new int[dimension*dimension][];
+		// int index = 0;
+		// for (int y = topBorder; y <= bottomBorder; y++){
+		// 	for(int x = rightBorder; x <= leftBorder; x++){
+		// 		for (int j = y - radius; j <= y + radius; j++){
+		// 			for (int i = x - radius; i <= x + radius; i++){
+		// 				int p = img.getRGB(j,i);
+		// 				int[] pixelValues = Utilities.pixelValues(p);
+		// 				pixels[index++] = pixelValues;
+		// 			}
+		// 		}
+		// 		int[] avgPixelValues = Utilities.avgPixelValues(pixels);
+		// 		int p = Utilities.setPixelValues(avgPixelValues);
+		// 		img.setRGB(y, x, p);//newImg.setRGB(y, x, p);
+		// 		index = 0;
+		// 	}
+		// }
+
+		// System.out.println("Done");
+
+		// // int[] test1 = {5,2,21,50};
+		// // int[] test2 = {6,4,43,100};
+		// // int[] test3 = {7,6,65,150};
+		// // int[] test4 = {8,8,87,200};
+		// // int[] test5 = {9,10,109,250};
+
+		// // int[] test = Utilities.medPixelValues(test1, test2, test3, test4);
+		// // for (int x : test)
+		// // 	System.out.print(" " + x);
+
+		// //Utilities.writeImage(newImg);
 		Utilities.writeImage(img);
-
-		int[] test1 = {5,2,21,50};
-		int[] test2 = {6,4,43,100};
-		int[] test3 = {7,6,65,150};
-		int[] test4 = {8,8,87,200};
-		int[] test5 = {9,10,109,250};
-
-		int[] test = Utilities.avgPixelValues(test1, test2, test3, test4, test5);
-		for (int x : test)
-			System.out.print(" " + x);
-
 
 	}
 
--- src/Utilities.java
+++ src/Utilities.java
@@ -2,12 +2,12 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Utilities{
-	public static BufferedImage readInImage(BufferedImage img){
+	public static BufferedImage readImage(BufferedImage img){
 		try{
 			img = ImageIO.read(new File("/home/sibusiso/Desktop/CSC2002S/Assignment 1/CSC2002S_Assignment_1/src/Puppy.jpeg"));
-			System.out.println("File read.");
 		}
 		catch(IOException e){
 			System.out.println("Error reading: " + e);
@@ -23,7 +23,8 @@ public class Utilities{
 	public static void writeImage(BufferedImage img){
 		try{
 			ImageIO.write(img, "jpeg", new File("/home/sibusiso/Desktop/CSC2002S/Assignment 1/CSC2002S_Assignment_1/src/PuppyOut.jpeg"));
-			System.out.println("File written");
+			System.out.println("Image written");
+			//home/sibusiso/Desktop/CSC2002S/Assignment 1/CSC2002S_Assignment_1/src
 		}
 		catch(IOException e){
 			System.out.println("Error writing: " + e);
@@ -69,9 +70,49 @@ public class Utilities{
 
 		// get averages of each ARGB values.
 		for (int i = 0; i < valuesAvg.length; i++)
-				valuesAvg[i] = valuesAvg[i]/Values.length;
+			valuesAvg[i] = valuesAvg[i]/Values.length;
 
 		return valuesAvg;
 
+	}
+
+	public static int[] medPixelValues(int[]... Values){
+		int[] valuesMed = new int[4];
+
+		int[] Avalues = new int[Values.length];
+		int[] Rvalues = new int[Values.length];
+		int[] Gvalues = new int[Values.length];
+		int[] Bvalues = new int[Values.length];
+		int ind = 0;
+
+		for (int[] pixel : Values){
+			Avalues[ind] = pixel[0];
+			Rvalues[ind] = pixel[1];
+			Gvalues[ind] = pixel[2];
+			Bvalues[ind++] = pixel[3];
+		}
+		
+		valuesMed[0] = getMedian(Avalues);
+		valuesMed[1] = getMedian(Rvalues);
+		valuesMed[2] = getMedian(Gvalues);
+		valuesMed[3] = getMedian(Bvalues);	
+
+		return valuesMed;		
+	}
+
+	public static int getMedian(int[] array){
+		Arrays.sort(array);
+		if (array.length % 2 == 1)
+			return array[array.length/2];
+		return (array[array.length/2] + array[(array.length/2) - 1])/2;
+	}
+
+	public static int[] getBorders(BufferedImage img, int dimension){
+		int[] borders = new int[4];
+		borders[0] = dimension/2; //top
+		borders[1] = img.getWidth() - dimension/2; //right
+		borders[2] = img.getHeight() - dimension/2; //bottom
+		borders[3] = dimension/2; //left
+		return borders;
 	}
 }
